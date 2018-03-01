57984f64769d13ba96fee4e70487bbb803c979b3
felicia mtoka <feliciahmtoka@gmail.com>
2018-03-01 11:49:48 +0300

    my fifth commit

---
+++ SwitchCase.js
@@ -0,0 +1,17 @@
+var isSunday = false
+
+switch(isSunday){
+	case true:
+	console.log("today is Sunday" )
+	break;
+
+	case false:
+	console.log("today is not Sunday")
+	break;
+
+	default:
+	console.log("error")
+	break;
+
+
+}
