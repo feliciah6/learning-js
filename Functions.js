57fc1308ef8a90662f77eaad7ad6fcab76d96955
felicia mtoka <feliciahmtoka@gmail.com>
2018-03-03 12:15:55 +0300

    my sixth commit

---
+++ Functions.js
@@ -0,0 +1,42 @@
+// this is a void function
+function add (){
+	var numberOne = 10;
+	var numberTwo = 20;
+	var sum = numberOne + numberTwo;
+
+	console.log(sum);
+
+}
+// call the Function
+add()
+function multiply(){
+	var numberOne = 30;
+	var numberTwo = 60;
+	var numberThree = 40;
+	var answer = numberOne * numberTwo * numberThree;
+	 return answer;
+	}
+var result = multiply();
+console.log(result );
+
+function subtraction(){
+
+	var numberOne = 20;
+	var numberTwo = 10;
+	var answer = numberOne - numberTwo;
+
+	  return answer;
+
+}
+var result = subtraction();
+console.log(result);
+ 
+
+ function divide(){
+ 	var numberOne = 20;
+ 	var numberTwo = 5;
+ 	var answer = numberOne / numberTwo;
+ 	 return answer;
+ }
+ var result = divide();
+ console.log(result);
