006f0585fe7950c175458e881296354a8921621a
felicia mtoka <feliciahmtoka@gmail.com>
2018-03-10 11:50:06 +0300

    my sixth commit

---
+++ Arrays.js
@@ -0,0 +1,6 @@
+var student = ["Jane","mary","laureen","ivy","fatma","judith","irene"]
+var shoeSize = [2, 4, 5, 6, 7, 19,20]
+// get the length ot the array
+
+for(i=0; i<student.length; i+=1)
+console.log(student[i]);
---
+++ FunctionParameters.js
@@ -0,0 +1,61 @@
+function areaOfTriangle(height, base){
+	var area = 0.5 * base * height;
+
+
+
+	return area;
+}
+
+var result = areaOfTriangle(40, 70)
+var result2 = areaOfTriangle(80, 40)
+var result3 = areaOfTriangle(60, 90)
+
+print(result)
+print(result2)
+print(result3)
+
+
+function print( answer){
+	console.log("Area of Triangle:" + answer)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
