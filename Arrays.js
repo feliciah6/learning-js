30ae95f5b351932925f95d95e49ece858f1d8a8b
felicia mtoka <feliciahmtoka@gmail.com>
2018-03-23 16:18:06 +0300

    my seventh commit

--- Arrays.js
+++ Arrays.js
@@ -2,5 +2,5 @@ var student = ["Jane","mary","laureen","ivy","fatma","judith","irene"]
 var shoeSize = [2, 4, 5, 6, 7, 19,20]
 // get the length ot the array
 
-for(i=0; i<student.length; i+=1)
+for(i=0; i<student.length; i++)
 console.log(student[i]);
